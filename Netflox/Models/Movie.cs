9a443c0addba31c27994bfb3b8d114c6e8427122
Yakuz0r <geoffrey_meunier@live.be>
2023-05-21 19:35:48 +0200

    database initialization

---
+++ Netflox/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace Netflox.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string nameMovie { get; set; }
+        public float recommendation { get; set; }
+        public string time { get; set; }
+        public string director { get; set; }
+        public string actor { get; set; }
+        public string synopsis { get; set; }
+
+
+
+
+    }
+}
--- Netflox/Pages/Privacy.cshtml.cs
+++ Netflox/Pages/Privacy.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Netflox.Data;
+using Netflox.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +13,20 @@ namespace Netflox.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        DataContext dataContext;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, DataContext dataContext)
         {
             _logger = logger;
+            this.dataContext = dataContext;
+            
         }
 
         public void OnGet()
         {
+            var movie = new Movie() { nameMovie = "Alien", time = "1h30"};
+            dataContext.Movies.Add(movie);
+            dataContext.SaveChanges();
         }
     }
 }
